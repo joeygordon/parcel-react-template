2ebd36e3bca9d13afc2de5f04aa2e43a2ebeb039
JoeyGordon <joey.gordon@gmail.com>
2019-07-06 15:42:58 -0700

    remove comments

--- src/components/Example.test.jsx
+++ src/components/Example.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import 'jest-dom/extend-expect';
 import Example from './Example';
 
 const wrapper = props => shallow(<Example {...props} />);
@@ -10,11 +9,9 @@ const props = {
 };
 
 test('should render Example', () => {
-  // const { asFragment } = component;
   expect(wrapper(props)).toBeTruthy();
 });
 
 test('should render Example with test message', () => {
-  // const { asFragment } = component;
   expect(wrapper(props).text()).toEqual('test message');
 });
